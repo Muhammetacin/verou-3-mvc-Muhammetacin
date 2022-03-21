c6c844a99548b294d0d556eefba5f1878bc76ea8
Muhammet Cin <muhammetacin@gmail.com>
2022-03-21 09:36:28 +0100

    Init project

---
+++ Starter-pack/View/articles/index.php
@@ -0,0 +1,14 @@
+<?php require 'View/includes/header.php'?>
+
+<?php // Use any data loaded in the controller here ?>
+
+<section>
+    <h1>Articles</h1>
+    <ul>
+        <?php foreach ($articles as $article) : ?>
+            <li><?= $article->title ?> (<?= $article->formatPublishDate() ?></li>
+        <?php endforeach; ?>
+    </ul>
+</section>
+
+<?php require 'View/includes/footer.php'?>
---
+++ Starter-pack/View/articles/show.php
@@ -0,0 +1,15 @@
+<?php require 'View/includes/header.php'?>
+
+<?php // Use any data loaded in the controller here ?>
+
+<section>
+    <h1><?= $article->title ?></h1>
+    <p><?= $article->formatPublishDate() ?></p>
+    <p><?= $article->description ?></p>
+
+    <?php // TODO: links to next and previous ?>
+    <a href="#">Previous article</a>
+    <a href="#">Next article</a>
+</section>
+
+<?php require 'View/includes/footer.php'?>
